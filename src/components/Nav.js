8bb2e5039fe09337c1f90d871e841d6509f84bfc
Eric Combee <45884566+ecombee28@users.noreply.github.com>
2021-12-10 18:02:08 -0700

    Fetch events from api 11

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "../Styles/Components_Style/nav.module.css";
 import logo from "../images/logo.png";
 import { useNavigate } from "react-router-dom";
+import Cookie from "js-cookie";
 
 const Nav = () => {
   const [userName, setUserName] = useState("");
@@ -9,8 +10,8 @@ const Nav = () => {
 
   useEffect(() => {
     const getUserName = () => {
-      let tokenKey = window.sessionStorage.getItem("token");
-      let user = window.sessionStorage.getItem("username");
+      let tokenKey = Cookie.get("token");
+      let user = Cookie.get("username");
 
       if (!tokenKey) {
         navigate("/");
@@ -23,10 +24,11 @@ const Nav = () => {
   });
 
   const signOut = () => {
-    window.sessionStorage.removeItem("username");
-    window.localStorage.removeItem("events");
-    window.sessionStorage.removeItem("token");
-    window.sessionStorage.removeItem("type");
+    Cookie.remove("username");
+    Cookie.remove("events");
+    Cookie.remove("token");
+    Cookie.remove("type");
+
     navigate("/");
   };
 
